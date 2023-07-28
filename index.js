10f718ea4f16a6457ac7cfda053dbf3893231dfc
Parva Jain <parv@Parvas-MacBook-Air.local>
2023-07-28 19:59:54 +0530

    removed logs

--- index.js
+++ index.js
@@ -22,29 +22,22 @@ async function smartContractInteraction() {
       id: 1,
     });
 
-    console.log("nonce ", web3.utils.toHex(nonce.data.result));
-
     // Encode the function signature
     const functionSignature = web3.utils.keccak256("set(uint256)").slice(0, 10);
-    console.log("functionSignature ", functionSignature);
 
     // Encode the function parameters
     const functionParameter = web3.eth.abi.encodeParameter("uint256", "50"); // setting value to 100
-    console.log("functionParameter ", functionParameter);
 
     // Concatenate the function signature and parameters to form the calldata
     const calldata = `${functionSignature}${functionParameter.slice(2)}`; // remove prefix 0x from functionParameter
-    console.log("calldata ", calldata);
 
     const gasEstimate = await web3.eth.estimateGas({
       from: myAccount,
       to: CONTRACT_ADDRESS,
       data: calldata,
     });
-    console.log("gasEstimate ", gasEstimate);
 
     const gasPrice = await web3.eth.getGasPrice();
-    console.log("gasPrice ", gasPrice);
 
     const txData = {
       from: myAccount,
@@ -59,11 +52,8 @@ async function smartContractInteraction() {
     const tx = new Tx(txData, { chain: "goerli" });
     tx.sign(privateKey);
 
-    console.log("tx ", tx);
     const serializedTx = tx.serialize();
-    console.log("serailized ", serializedTx);
     const rawTx = "0x" + serializedTx.toString("hex");
-    console.log("rawTx ", rawTx);
 
     const txReceipt = await axios.post(INFURA_URL, {
       jsonrpc: "2.0",
